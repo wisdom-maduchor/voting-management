39e92fa256f7b89155a3249f01d113dce96670df
wisdom-maduchor <maduchorwisdom@gmail.com>
2025-04-17 17:26:26 +0100

    push

--- app/Filament/Pages/VotingAdminDashboard.php
+++ app/Filament/Pages/VotingAdminDashboard.php
@@ -9,5 +9,15 @@ class VotingAdminDashboard extends Page
     // protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static ?string $navigationIcon = 'heroicon-o-home';
 
+    // Set the custom navigation label here.
+    protected static ?string $navigationLabel = ' Dashboard ';
+
+    // Set the view for this page.
     protected static string $view = 'filament.pages.voting-admin-dashboard';
+
+    // Override to remove the header completely.
+    public function getHeading(): string
+    {
+        return '';
+    }
 }
